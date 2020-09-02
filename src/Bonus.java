5860fe04407b6988a0db0906f87fa95d1a4483a3
AlenaKorSpb <evtam-29@mail.ru>
2020-09-02 23:46:41 +0300

    added f.Bonus.java

---
+++ src/Bonus.java
@@ -0,0 +1,14 @@
+public class Bonus {
+    public static void main(String[] args) {
+
+        int rub;
+
+            rub = 1000;
+            long amount = 15230;
+            long bonus = amount / 100;
+
+
+
+                System.out.println(bonus);
+            }
+}
