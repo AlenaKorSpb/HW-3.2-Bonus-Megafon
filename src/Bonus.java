f31e7a484d264894769b00554f9f0946b63ed0cb
AlenaKorSpb <evtam-29@mail.ru>
2020-09-03 21:01:46 +0300

    change f.Bonus

--- src/Bonus.java
+++ src/Bonus.java
@@ -2,13 +2,21 @@ public class Bonus {
     public static void main(String[] args) {
 
         int rub;
-
-            rub = 1000;
-            long amount = 15230;
-            long bonus = amount / 100;
+            rub = 100;
 
 
+        long amount = 1001;
+        long bonus = amount / rub;
+        long limit = 1000;
 
-                System.out.println(bonus);
-            }
+
+        if (amount > limit) {
+            bonus = amount / rub;
+        }else{
+            bonus = 0;
+        }
+
+
+        System.out.println(bonus);
+    }
 }
